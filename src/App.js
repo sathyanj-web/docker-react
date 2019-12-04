7f475fec286e278fc28e15e31fe7440c02ae3310
Sathya NJ <sathyanj2@gmail.com>
2019-12-04 19:36:53 +0530

    modified app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React now !
+          Learn React now once again !
         </a>
       </header>
     </div>
